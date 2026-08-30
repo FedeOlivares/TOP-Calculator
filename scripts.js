5b47a5cc6b7dcf80514e91e207c99132560f5036
Federico Olivares <FedeOlivares@users.noreply.github.com>
2026-08-30 20:44:06 -0300

    v0.2 / added basic screen function
    modified:   index.html
    modified:   scripts.js
    modified:   styles.css

--- scripts.js
+++ scripts.js
@@ -1,15 +1,68 @@
-const digits = ["+", "-", "x", "/", "7", "8", "9",".", "4", "5", "6","=", "1", "2", "3", "C", "0"];
+const buttonDefs = new Map([
+  ["+", { type: "operator" }],
+  ["-", { type: "operator" }],
+  ["x", { type: "operator" }],
+  ["/", { type: "operator" }],
+  ["7", { type: "numeric" }],
+  ["8", { type: "numeric" }],
+  ["9", { type: "numeric" }],
+  [".", { type: "numeric" }],
+  ["4", { type: "numeric" }],
+  ["5", { type: "numeric" }],
+  ["6", { type: "numeric" }],
+  ["C", { type: "action" }],
+  ["1", { type: "numeric" }],
+  ["2", { type: "numeric" }],
+  ["3", { type: "numeric" }],
+  ["=", { type: "action" }],
+  ["0", { type: "numeric" }]
+]);
+
+let currentInputA = "";
+let currentInputB = "";
+let currentInputOpr = "";
+
 
 const btnGrid = document.querySelector(".calculatorGrid");
+const input = document.getElementById("input");
+const output = document.getElementById("output");
 
 
-digits.forEach(digit => {
+buttonDefs.forEach((def, symbol) => {
   const btn = document.createElement("button");
-  btn.textContent = digit;
+  btn.textContent = symbol;
   btn.classList.add("btn");
-  if (digit === "0") {
+  btn.dataset.type = def.type;
+  if (symbol === "0") {
     btn.classList.add("zero");
   }
   btnGrid.appendChild(btn);
 });
 
+const buttons = document.querySelectorAll(".btn")
+
+buttons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    const val = btn.textContent;
+    const type = btn.dataset.type;
+
+    if (type === "numeric") {
+      if (currentInputOpr === "") {
+        currentInputA += val;
+        input.textContent = currentInputA;
+      } else {
+        currentInputB += val;
+        input.textContent = currentInputB;
+      }
+    } else if (type === "operator") {
+        currentInputOpr = val;
+        input.textContent = currentInputOpr;
+      }
+    else if (val === "C") {
+      currentInputA = "";
+      currentInputB = "";
+      currentInputOpr = "";
+    }  
+    input.textContent = currentInputA + currentInputOpr + currentInputB;   
+  });
+});
